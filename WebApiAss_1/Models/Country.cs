ead41a6cd36e56d48c2e491106c1da1d73034990
Harish S <80798474+Harish041@users.noreply.github.com>
2022-11-23 01:22:23 +0530

    Assignment 1 Web Api

---
+++ WebApiAss_1/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiAss_1.Models
+{
+    public class Country
+    {
+        [Key]
+        public int id { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+    }
+}
